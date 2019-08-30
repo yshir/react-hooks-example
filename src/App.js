f0fa88a8b6fd5485850f664f38f0ad575a4e9590
yshir <yshir0020@gmail.com>
2019-08-31 00:31:31 +0900

    install bulma

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 import React from 'react';
+import '../node_modules/bulma/css/bulma.css';
 
-export default () => <div>Hi</div>;
+export default () => (
+  <div className="container">
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">イベント作成アプリケーション</h1>
+          <h2 className="subtitle">with React hooks</h2>
+        </div>
+      </div>
+    </section>
+  </div>
+);
