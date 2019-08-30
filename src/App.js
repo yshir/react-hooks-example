2e6e55ea8f7a33e2f674cd1bf6d11d80b4abc80e
yshir <yshir0020@gmail.com>
2019-08-31 00:34:33 +0900

    create actions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../node_modules/bulma/css/bulma.css';
 
-export default () => (
-  <div className="container">
-    <section className="hero is-primary">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">イベント作成アプリケーション</h1>
-          <h2 className="subtitle">with React hooks</h2>
+export default () => {
+  return (
+    <div className="container">
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">イベント作成アプリケーション</h1>
+            <h2 className="subtitle">with React hooks</h2>
+          </div>
         </div>
-      </div>
-    </section>
-  </div>
-);
+      </section>
+    </div>
+  );
+};
